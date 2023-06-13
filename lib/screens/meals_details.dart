e9c250ca61b4e51308ddcd42661aae796fc2ec7c
Mayur <kmayur819@gmail.com>
2023-06-13 22:30:25 +0530

    added some details in meals details screeen

--- lib/screens/meals_details.dart
+++ lib/screens/meals_details.dart
@@ -10,15 +10,46 @@ class MealsDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
-      ),
-    );
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+              ),
+              for (final ingredients in meal.ingredients)
+                Text(
+                  ingredients,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.background,
+                      ),
+                ),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                "steps",
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+              ),
+              for (final steps in meal.steps) Text(steps)
+            ],
+          ),
+        ));
   }
 }
