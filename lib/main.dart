c3399bf83583fac94bedc6632cb4472028f09e1e
Mayur <kmayur819@gmail.com>
2023-06-21 19:22:27 +0530

    added dart fixes that remove the unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mealsapp/screens/categories.dart';
 import 'package:mealsapp/screens/tabs.dart';
 
 final theme = ThemeData(
